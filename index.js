c157b51cc834099182f4e58ca1f020019f06c194
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-12 01:48:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function johnLennonFacts(facts){
   }
   return array
 }
-/** The function should create a variable that stores an empty array. 
+/** The function should create a variable that stores an empty array.
 * Implement a do-while loop inside the function that adds `"I love the
-  Beatles!"` to the empty array. 
+  Beatles!"` to the empty array.
 * The loop should then increment the number passed in as a parameter. The
   condition of the loop should check to see that the parameter number is
   less than `15`. It should contain a number of strings that represents the
@@ -30,12 +30,3 @@ function iLoveTheBeatles(num){
     ++num
   } while (num < 15)
 }
-
-
-
-
-
-
-
-
-
