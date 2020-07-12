8372a5d8aa8dc3b4c43d49feff2640ff9a7a2e65
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-12 01:30:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for ( let i = 0; i < musicians.length; i++ ){
+    array.push (`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  var array = []
+  for (let i = 0; i < facts.length; i++){
+    array.push(`${facts[i]}!!!`)
+  }
+  return array
+}
